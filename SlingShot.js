6e4d30eccd0f2be0b90ee832170f8522dffc0aea
Leelu12 <72986748+Leelu12@users.noreply.github.com>
2020-12-07 19:02:51 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,28 @@
+class SlingShot{
+//connect the body to a point
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        //display only if the bird position is not null
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    //break the connection of the bird,null means nothing
+    fly(){
+   this.sling.bodyA=null
+ }
+}
